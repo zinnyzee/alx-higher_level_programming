1b1d8cb2dfbf1f63e1fce3d6d9957507c923cc0a
nduka <ndukaabigail1@gmail.com>
2022-07-18 17:57:59 -0700

    commit all 0x05

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
+
 def safe_print_division(a, b):
     try:
-        result = a / b
-    except:
-        result = None
+        res = a / b
+    except ZeroDivisionError:
+        res = "None"
+        return None
     finally:
-        print("Inside result: {}".format(result))
-        return (result)
+        print("Inside result: {}".format(res))
+    return res
